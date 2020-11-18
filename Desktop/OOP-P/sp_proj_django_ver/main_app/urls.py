bd1341dba78cb3ec23b3fd2d53b35f0dff82f634
mhrabbi <mhrabby61@gmail.com>
2020-11-18 15:23:50 +0600

    'UP'

---
+++ Desktop/OOP-P/sp_proj_django_ver/main_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home_view,name="home"),
+    path('show_info/',views.show_info,name="show_info"),
+    path('show_info/student_info/',views.student_info,name="student_info"),
+    path('search_info/',views.search_info,name="search_info"),
+    path('search_info/single_info/',views.search_by_id,name="search_by_id"),
+    path('show_all_students/',views.show_all_student,name="show_all_students"),
+    path('del_registration/',views.del_registration,name="del_registration"),
+    path('del_registration/del_id/',views.del_reg_main,name="del_reg_main")
+]
