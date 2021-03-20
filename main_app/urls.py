978e54fbc70f5dcb4b4e100a6cdb16e1df5cdd59
moshiulrabbi <mhrabby61@gmail.com>
2021-03-20 20:38:57 +0600

    model-converted

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,15 +1,35 @@
+from os import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home_view,name="home"),
-    path('show_info/',views.show_info,name="show_info"),
-    path('show_info/student_info/',views.student_info,name="student_info"),
-    path('search_info/',views.search_info,name="search_info"),
-    path('search_info/single_info/',views.search_by_id,name="search_by_id"),
+    # path('',views.home,name="home"),
+    # path('add_reg/',views.add_reg,name="add-reg"),
+    # path('all_student/',views.all_student,name="all_student"),
+    # path('search',views.search,name="search"),
+
+    path('',views.home,name="home"),
+    path('search/',views.search,name="search"),
+    path('about/',views.about,name="about"),
     path('show_all_students/',views.show_all_student,name="show_all_students"),
-    path('del_registration/',views.del_registration,name="del_registration"),
-    path('del_registration/del_id/',views.del_reg_main,name="del_reg_main"),
-    path('about_us/',views.about_us,name="about_us"),
+    path('add_student',views.add_student,name="add_student"),
+
+
+
+
+
+
+
+
+
+    # path('',views.home_view,name="home"),
+    # path('show_info/',views.show_info,name="show_info"),
+    # path('show_info/student_info/',views.student_info,name="student_info"),
+    # path('search_info/',views.search_info,name="search_info"),
+    # path('search_info/single_info/',views.search_by_id,name="search_by_id"),
+    # path('show_all_students/',views.show_all_student,name="show_all_students"),
+    # path('del_registration/',views.del_registration,name="del_registration"),
+    # path('del_registration/del_id/',views.del_reg_main,name="del_reg_main"),
+    # path('about_us/',views.about_us,name="about_us"),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,24 +5,71 @@ from django.urls import reverse
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-import json
+from .models import StudentInfo
+from django.views.decorators.csrf import csrf_exempt
 
 
-def home_view(request):
+
+def home(request):
     return render(request, "home.html")
 
-        
 
-def show_info(request):
-    return render(request, "add_reg.html")
+@csrf_exempt
+def add_student(request):
+
+    if request.method == "POST":
+
+        name = request.POST["name"]   
+        id = request.POST["id"]
+        department=request.POST["department"]
+        section = request.POST["section"]
+        payment=request.POST["payment"]
+
+        if name=="" or id =="" or section=="":
+            msg = "Please Enter Name,Id and Section"
+
+        elif StudentInfo.objects.filter(id=id).exists():
+            msg = "ID Already Exists!"
+        else:
+            student = StudentInfo(name=name,id=id,department=department,section=section,payment=payment)
+            student.save()
+            msg = "Student registered Sucessfully"
+
+        return render(request, "add_reg.html", {"msg": msg})
+    else:
+        return render(request, "add_reg.html")
 
 
-def search_info(request):
-    return render(request,"search_info.html")
 
 
 
-def about_us(request):
+
+
+
+def show_all_student(request):
+    students = StudentInfo.objects.all()
+    c = StudentInfo.objects.count()
+    return render(request,"show_all_student.html",{"student":students,"c":c})
+
+
+
+
+
+
+
+def search(request):
+    query = request.GET.get("id")
+    
+    if query:
+        s = StudentInfo.objects.all()
+        student = s.filter(id__contains=query)
+        return render(request,"single_info.html",{"student":student})
+    else:
+        return render(request,"search_info.html")
+
+
+
+def about(request):
     return render(request,"about_us.html")
 
 
@@ -43,112 +90,12 @@ def login_view(request):
 '''
 
 
-def search_by_id(request):
-    def read_file():
-        try:
-            with open("info.json", "r") as project_json:
-                file = project_json.read()
-                info_list = json.loads(file)
-                project_json.close()
-                return info_list
-
-        except:
-            return []
-
-    
-    ID = request.GET["id"]
-    file = read_file()
-    id_list = []
-    for item in file:
-        id_list.append(item["ID"])
-    if ID in id_list:
-        for item in file:
-            if item["ID"] == ID:
-                msg=item
-    else:
-        msg="Student not Registered or Invalid ID."
-    return render(request,"single_info.html",{"info":msg,"id_list":id_list,"id":ID})
 
 
 
 
-def show_all_student(request):
-    def read_file():
-        try:
-            with open("info.json", "r") as project_json:
-                file = project_json.read()
-                info_list = json.loads(file)
-                project_json.close()
-                return info_list
 
-        except:
-            return []
-
-    all_student=read_file()
-    num_of_students=len(all_student)
-
-
-    return render(request,"show_all_student.html",{"all_student":all_student,"num_of_students":num_of_students})
-
-
-
-def student_info(request):
-
-    if request.method == "POST":
-
-
-        def read_file():
-            try:
-                with open("info.json", "r") as project_json:
-                    file = project_json.read()
-                    info_list = json.loads(file)
-                    project_json.close()
-                    return info_list
-
-            except:
-                return []
-
-
-        def write_file(array):
-            with open("info.json", "w+") as project_json:
-                content = json.dumps(array, indent=4)
-                project_json.writelines(content)
-                project_json.close()
-
-
-        file = read_file()
-        id_list = []
-        for item in file:
-            id_list.append(item["ID"])
-
-        name = request.POST["name"]   
-        ID = request.POST["id"] 
-        section = request.POST["section"]
-        dep=request.POST["department"]
-        payment=request.POST["payment"]
-
-        info_dic = {
-            "Name": name,
-            "ID": ID,
-            "Section": section,
-            "Department":dep,
-            "Payment_Status":payment,
-        }
-        if ID in id_list:
-            msg="ID already Registered !"
-        else:
-            if name=="" or ID=="" or section=="":
-                msg="Please Enter Name, ID and Section"
-            else:
-                student_info = read_file()
-                student_info.append(info_dic)
-                write_file(student_info)
-
-                msg = "Registration Complete !! "
-        return render(request, "add_reg.html", {"msg": msg})
-    else:
-        return render(request, "add_reg.html")
-
+#aDD STUDENT        
     
 
 '''
@@ -160,58 +107,9 @@ def logout_view(request):
 
 
 
-def del_registration(request):
-    
-    return render(request,"del_registration.html")
 
 
 
-
-
-def del_reg_main(request):
-    def read_file():
-        try:
-            with open("info.json", "r") as project_json:
-                file = project_json.read()
-                info_list = json.loads(file)
-                project_json.close()
-                return info_list
-
-        except:
-            return []
-
-    def write_file(array):
-        with open("info.json", "w+") as project_json:
-            content = json.dumps(array, indent=4)
-            project_json.writelines(content)
-            project_json.close()
-
-    ID=request.GET["id"]
-    my_file=read_file()
-    id_list=[]
-    new_data=[]
-
-    for item in my_file:
-        id_list.append(item["ID"])
-    
-    if ID== "":
-        msg="Please Enter an ID!!"
-    else:
-    
-        if ID not in id_list:
-            msg="ID Not Registered"
-        else:
-            
-            for item in my_file:
-                if item["ID"] == ID:
-                    deleted=item
-                    continue
-                else:
-                    new_data.append(item)
-            write_file(new_data)
-            msg="Information Deleted Successfully !!"
-        
-    return render(request,"del_reg_main.html",{"msg":msg})
 
 
 
